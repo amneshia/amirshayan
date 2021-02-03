1268ceb5739e24145a501253599f23506c3d7ddc
Amir Shayan Armaghan <amirshayan.armaghan@gmail.com>
2021-02-03 18:47:26 -0500

    remove usersRout
    - also changed let to const

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET Home page. */
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+/* GET About Me page. */
+router.get('/about', (req, res, next) => {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET Projects page. */
+router.get('/projects', (req, res, next) => {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', (req, res, next) => {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('index', { title: 'Contact Me' });
 });
 
 module.exports = router;
