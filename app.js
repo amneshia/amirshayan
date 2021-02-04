cd9a44c13477d97c15cf771caa4de4233319d9cd
Amir Shayan Armaghan <amirshayan.armaghan@gmail.com>
2021-02-04 00:36:17 -0500

    minor improvements
    -update main_nav
    -convert all occurances of 'function' to arrow notation.
    -remove unnessecary users route file and reference

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -19,28 +18,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-app.use(function(req, res, next) {  
-  res.locals.req = req;
+app.use((req, res, next) => {
+  res.locals.request = req;
   res.locals.brandName = "<amirshayan/>";
   res.locals.lang = "en";
-  next()
+  next();
 });
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {title: "Error"});
+  res.render('error', { title: "Error" });
 });
 
 module.exports = app;
