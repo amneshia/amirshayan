765f7dc1b6af72c7003c99deef27b9b630835c85
Amir Shayan Armaghan <amirshayan.armaghan@gmail.com>
2021-02-10 18:15:09 -0500

    implement projects page

--- repositories/projectrepo.js
+++ repositories/projectrepo.js
@@ -9,9 +9,27 @@ class ProjectRepo {
                 `"Tezol Market" is the online groceries app operating in Iran. It allows users 
                 to order groceries online from a nearby store of their choosing, and have them 
                 delivered to their specified address at specified time.`,
-                "/images/tezolmarket.png",
-                "www.tezolmarket.com",
-                "Lead Android software engineer",
+                "/images/tezolmarket.jfif",
+                "https://www.tezolmarket.com",
+                "Lead Android software & search engine developer"
+            ),
+            new Project(
+                "purescript-rxjs",
+                `"purescript-rxjs" is the implementation of "Reactive Streams" in the "Purescript" language. The library allows writing 
+                 streaming pipelines and reactive software based based on functional 
+                 programming combinators in an expressive and declarative way.`,
+                "/images/purescript-rxjs.png",
+                "https://github.com/amneshia/purescript-rxjs",
+                "Source library & testing contributor"
+            ),
+            new Project(
+                "tennis-kata",
+                `"tennis-kata" is the implementation of the "Tennis Code Kata" in Haskell. Haskell is my No.1 favorite programming language. 
+                Given Haskell's immutable and purely functional nature, in this tutorial library I demonstrate how one can model change of 
+                state in Haskell utilizing the "State Monad".`,
+                "/images/tennis-kata.png",
+                "https://github.com/amneshia/tennis-kata",
+                "Source library author"
             )
         ];
     }
