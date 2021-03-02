00d5ed64f2d2a31b6ade8c0ae67a9b4bb5c37e85
Amir Shayan Armaghan <amirshayan.armaghan@gmail.com>
2021-03-02 06:30:28 -0500

    update the config file for atlas usage

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 module.exports = function() {
-    const db = mongoose.connect('mongodb://localhost/amirshayan');
+    // const uri = 'mongodb://localhost/amirshayan'; 
+    const uri = "mongodb+srv://amirshasyan:5DCVExiSiz18jvKm@cluster0.30uuz.mongodb.net/amirshayan?retryWrites=true&w=majority"; 
+    const db = mongoose.connect(uri);
     require('../models/schemas/user.');
     require('../models/schemas/contact');
     return db;
