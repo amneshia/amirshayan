88f379525725d7241894eeb55d0034a665a60972
Amir Shayan Armaghan <amirshayan.armaghan@gmail.com>
2021-03-02 22:40:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
+//New additions since last presentation
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const User = require('mongoose').model('User');
 const flash = require("connect-flash");
+const LocalStrategy = require('passport-local').Strategy;
 const {methodOverride, globalLocals} = require("./middleware/middlewares");
 
 const app = express();
@@ -19,22 +20,22 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride);
-app.use(session({ secret: "cats" }));
+app.use(methodOverride); //new
+app.use(session({ secret: "cats" })); //new
 app.use(cookieParser());
-app.use(flash());
+app.use(flash()); //new
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 // Passport setup
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize()); //new
+app.use(passport.session()); //new
 passport.serializeUser(function (user, done) {
   done(null, user);
-});
+});//new
 passport.deserializeUser(function (user, done) {
   done(null, user);
-});
+});//new
 passport.use(new LocalStrategy(
   function (username, password, done) {
     User.findOne({ username: username }, function (err, user) {
@@ -44,9 +45,9 @@ passport.use(new LocalStrategy(
       return done(null, user);
     });
   }
-));
+));//new
 
-app.use(globalLocals);
+app.use(globalLocals); //new
 app.use('/', indexRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
